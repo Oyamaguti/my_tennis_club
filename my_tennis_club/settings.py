27b741fc2c973ef149de2dcf4d236b2675e42186
oscar.taniguchi <oscar.taniguchi@tecnogroup.com.br>
2025-07-03 18:54:32 -0300

    integracao vercel 5

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -76,11 +76,22 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.app'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv("POSTGRES_DATABASE"),
+    'USER': os.getenv("POSTGRES_USER"),
+    'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+    'HOST': os.getenv("POSTGRES_HOST"),
+    'PORT': os.getenv("POSTGRES_DB_PORT") if "POSTGRES_DB_PORT" in os.environ.keys() else 5432,
+  }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
